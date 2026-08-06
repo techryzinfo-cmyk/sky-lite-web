7785b48fd9c439dc91ee9f09c79295fefcd49ef4
Maheshkadam-Delxn <maheshkadam.delxn@gmail.com>
2026-08-06 09:57:30 +0530

    fix: update API destination variable in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_API_BASE_URL
-          ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`
+        destination: process.env.NEXT_PUBLIC_API_URL
+          ? `${process.env.NEXT_PUBLIC_API_URL}/:path*`
           : 'http://localhost:3000/api/:path*',
       },
     ];
